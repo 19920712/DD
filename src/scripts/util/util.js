f727636f3a267492173b27c48845bdaa2f5f4330
unknown <>
2016-08-16 15:01:56 +0800

    正常

--- src/scripts/util/util.js
+++ src/scripts/util/util.js
@@ -1,12 +1,6 @@
 var util={
 	setFocus:function(e){
 		e.addClass('active').siblings().removeClass('active')
-	},
-	aa:function(){
-		console.log("master");
-	},
-	bb:function(){
-		console.log("20160815w");
 	}
 }
 module.exports=util;
